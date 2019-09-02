449bf55729aaafab43b17c523211cce5a273b759
fangchen <fxf432@163.com>
2019-09-02 14:43:55 +0800

    21

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+    }
+}
+
