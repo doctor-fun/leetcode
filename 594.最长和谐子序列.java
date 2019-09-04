97367f288aee2a65d61b882c84b7481428cb9e05
fangchen <fxf432@163.com>
2019-09-04 17:07:44 +0800

    594

--- 594.最长和谐子序列.java
+++ 594.最长和谐子序列.java
@@ -6,7 +6,8 @@ import java.util.Map;
  *
  * [594] 最长和谐子序列
  */
-class Solution1 {
+class Solution {
+    //用hashmap统计所有的数的频次，需要用的getOrDefault,然后遍历所有相差为1的组合的数的个数，如1和2,2和3，留一个max叫longest
     public int findLHS(int[] nums) {
      
        // getOrDefault
@@ -14,7 +15,7 @@ class Solution1 {
             //则返回第二个参数即默认值。要注意，这个默认值不会放入 Map
             Map<Integer, Integer> countForNum = new HashMap<>();
             for (int num : nums) {
-                countForNum.put(num, countForNum.getOrDefault(num, 0) + 1);
+                countForNum.put(num, countForNum.getOrDefault(num, 0) + 1);//没有就value=0+1=1,有旧value+1//看来是数这个数的个数的
             }
             int longest = 0;
             for (int num : countForNum.keySet()) {
@@ -25,14 +26,14 @@ class Solution1 {
             return longest;
         }
     
-}
-class Main{
-    public static void main(String[] args){
-    Solution1 solution=new Solution1();
-    int[] nums={1,3,2,2,5,2,3,7};
-    int result=solution.findLHS(nums);
-    System.out.println(result);   
-}
+// }
+// class Main{
+//     public static void main(String[] args){
+//     Solution solution=new Solution();
+//     int[] nums={1,3,2,2,5,2,3,7};
+//     int result=solution.findLHS(nums);
+//     System.out.println(result);   
+// }
 }
 
 
