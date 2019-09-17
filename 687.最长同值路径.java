9e785723cc3e5f8ed134573bd75921934675f541
fangchen <fxf432@163.com>
2019-09-17 17:32:59 +0800

    687

---
+++ 687.最长同值路径.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=687 lang=java
+ *
+ * [687] 最长同值路径
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//这里提到最长最短，说明要加个Max的flag
+//最多判断7个节点，判断之
+class Solution {
+    private int max=0;
+    public int longestUnivaluePath(TreeNode root) {
+        
+    }
+}
+
