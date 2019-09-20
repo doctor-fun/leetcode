38408a6f5edab31f061487a2ab638e5f2d9e60ec
chenfang <fxf432@163.com>
2019-09-20 23:52:34 +0800

    20

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -6,6 +6,7 @@ import java.util.Stack;
  * [20] 有效的括号
  */
 //进一个判断一个，判断条件是只要一个符合，就不是有效的括号，要考虑进的东西不是3种括号之一
+//如果遇到反括号，就出栈一个，和反括号做对比，看看是否匹配；
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
