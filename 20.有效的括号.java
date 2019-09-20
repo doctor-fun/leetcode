8cf6eb877833ad224d42401f8e167a4049ff1643
chenfang <fxf432@163.com>
2019-09-20 23:50:11 +0800

    20

---
+++ 20.有效的括号.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+//进一个判断一个，判断条件是只要一个符合，就不是有效的括号，要考虑进的东西不是3种括号之一
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+            for(char c :s.toCharArray()){
+                if(c=='(' ||c=='{'||c=='['){
+                    stack.push(c);
+                }
+               else { if(stack.isEmpty()) {return false;}
+               char cStack=stack.pop();
+                boolean b1=c==')'&& cStack!='(';
+                boolean b2=c==']'&& cStack!='[';
+                boolean b3=c=='}' && cStack!='{';
+                if(b1||b2||b3){
+                    return false;
+                }
+            }
+            }
+            return stack.isEmpty();
+    }
+}
+
